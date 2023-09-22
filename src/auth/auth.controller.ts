71589b0b012d281c674090b3187b939bad34fa74
Andy Rahman <andyrahman989@gmail.com>
2023-09-22 15:18:31 +0800

    adding login, get authenticated user, and logout in auth controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,12 +1,14 @@
 import { registerDTO } from './validation/register.dto';
-import { BadRequestException, Body, Controller, Post, Res } from '@nestjs/common';
+import { BadRequestException, Body, ClassSerializerInterceptor, Controller, Get, Post, Req, Res, UseGuards, UseInterceptors } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { UserService } from 'src/user/user.service';
 import { AuthService } from './auth.service';
 import { Response, Request } from 'express';
 import * as argon2 from 'argon2';
 import { JoiValidationPipe } from 'src/common/validation.pipe';
+import { AuthGuard } from './auth.guard';
 
+@UseInterceptors(ClassSerializerInterceptor) // hide the password
 @Controller()
 export class AuthController {
     constructor(
@@ -20,13 +22,13 @@ export class AuthController {
         @Body(new JoiValidationPipe(registerDTO)) body: any,
         @Res({ passthrough: true }) response: Response
     ) {
-        const existingEmail = await this.userService.findOne({email: body.email});
+        const existingEmail = await this.userService.findOne({ email: body.email });
 
         if (existingEmail) {
             throw new BadRequestException('Email already exists');
         }
 
-        const existingUsername = await this.userService.findOne({username: body.username});
+        const existingUsername = await this.userService.findOne({ username: body.username });
 
         if (existingUsername) {
             throw new BadRequestException('Username already exists');
@@ -44,5 +46,68 @@ export class AuthController {
         });
     }
 
-    
+    @Post('login')
+    async login(
+        @Body('username') username: string,
+        @Body('email') email: string,
+        @Body('password') password: string,
+        @Res({ passthrough: true }) response: Response,
+        @Body('rememberMe') rememberMe?: boolean
+    ) {
+        let user;
+
+        // Check whether to find the user by email or username based on input.
+        if (email) {
+            user = await this.userService.findOne({ email: email });
+        } else {
+            user = await this.userService.findOne({ username: username });
+        }
+
+        // If user doesn't exist, throw a BadRequestException indicating invalid credentials.
+        if (!user) {
+            throw new BadRequestException('Username or Email is Invalid');
+        }
+
+        if (!await argon2.verify(user.password, password)) {
+            throw new BadRequestException("Password is invalid")
+        }
+
+        // Calculate the expiration time for the refresh token
+        // Generate a refresh token using the JWT service with the calculated expiration time.
+        const refreshTokenExpiration = rememberMe
+            ? new Date(Date.now() + 365 * 24 * 60 * 60 * 1000) // 1 year from now
+            : new Date(Date.now() + 7 * 24 * 60 * 60 * 1000); // Adding 7 days in milliseconds
+
+        const jwt = await this.jwtService.signAsync({ id: user.id });
+
+        response.cookie('user_session', jwt, {
+            httpOnly: true,
+            expires: refreshTokenExpiration,
+        });
+        response.status(200);
+
+        return user;
+    }
+
+    @UseGuards(AuthGuard)
+    @Get('user')
+    async user(
+        @Req() request: Request,
+    ) {
+        const id = await this.authService.userId(request);
+
+        return this.userService.findOne({ id });
+    }
+
+    @UseGuards(AuthGuard)
+    @Post('logout')
+    async logout(
+        @Res({ passthrough: true }) response: Response
+    ) {
+        response.clearCookie('user_session');
+
+        return {
+            message: "success"
+        }
+    }
 }
--- src/auth/auth.guard.ts
+++ src/auth/auth.guard.ts
@@ -1,11 +1,23 @@
-import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
-import { Observable } from 'rxjs';
+import { CanActivate, ExecutionContext, Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 
+// Check if the user is Authenticated
 @Injectable()
 export class AuthGuard implements CanActivate {
-  canActivate(
-    context: ExecutionContext,
-  ): boolean | Promise<boolean> | Observable<boolean> {
-    return true;
+  constructor(private jwtService: JwtService) { }
+
+  canActivate(context: ExecutionContext) {
+    const request = context.switchToHttp().getRequest();
+    const jwt = request.cookies['user_session'];
+
+    if (!jwt) {
+      throw new UnauthorizedException('User not authenticated');
+    }
+
+    try {
+      return this.jwtService.verify(jwt);
+    } catch (error) {
+      throw new UnauthorizedException('User not authenticated');
+    }
   }
 }
